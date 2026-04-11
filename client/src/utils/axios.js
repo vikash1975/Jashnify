7a2a89a87da08da34748e3c7f4fb3f610c95d0cf
vikash1975 <167326024+vikash1975@users.noreply.github.com>
2026-04-11 16:40:33 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    // baseURL: 'http://localhost:5000/api',
+      baseURL: 'https://jashnify-backend-rl23.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
